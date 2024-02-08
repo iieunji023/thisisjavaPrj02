4da8cf5d0760c5f2740a7781737fbd19b02385bd
iieunji023 <iieunji023@gmail.com>
2024-02-08 18:06:31 +0900

    과제

--- src/com/eunji/buy/CustomClass.java
+++ src/com/eunji/buy/CustomClass.java
@@ -6,20 +6,47 @@ public class CustomClass {
 	String name;		// 회원 이름
 	int age;			// 회원 나이
 	
+	/*
+	 * 물품 객체가 저장될 장바구니
+	 * 물품이 담기는 타입이 ProductClass객체이므로 ProductClass객체 타입의 배열 형태로 저장함
+	 */
+	ProductClass[] shoppingBasket;
+	
 	// 생성자 선언
-	public CustomClass(int id, String name, int age) {
+	public CustomClass(int id, String name, int age, ProductClass[] shoppingBasket) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
+		this.shoppingBasket = shoppingBasket;
+		
 	}
+	
+//	public CustomClass(int quantity) {
+//		p.quantity = quantity;
+//		
+//	}
 	
 	// 초기화가 잘 이루어졌는지 확인하기 위한 메소드
 	void customInfo() {
-//		System.out.println("id: " + id);
-//		System.out.println("name: " + name);
-//		System.out.println("age: " + age);
-		
 		System.out.printf("id: %d, name: %s, age: %d \n", id, name, age);
+		
+	}
+	
+	// 장바구니에 담는 메소드
+	void addToCart(ProductClass p, int quantity) {
+		p.quantity = quantity;
+		// 배열에 product 객체 저장
+//		shoppingBasket = new ProductClass[0] {p};
+		
+		int i = 0;
+		shoppingBasket[i] = p;
+		i++;
+		shoppingBasket[i] = p;
+		
+		
+		
+		
+//		shoppingBasket[product.id]{product, quantity};
 		
 	}
 	
--- src/com/eunji/buy/ExecClass.java
+++ src/com/eunji/buy/ExecClass.java
@@ -3,9 +3,9 @@ package com.eunji.buy;
 public class ExecClass {
 	public static void main(String[] args) {
 		/*
-		 * 회원 등록
+		 * 1. 회원 등록
 		 */
-		CustomClass c1 = new CustomClass(1, "이자바", 25);
+		CustomClass c1 = new CustomClass(1, "이자바", 25, new ProductClass[10]);
 		CustomClass c2 = new CustomClass(2, "박자바", 22);
 		CustomClass c3 = new CustomClass(3, "이은지", 27);
 		
@@ -14,18 +14,32 @@ public class ExecClass {
 		c3.customInfo();
 		
 		/*
-		 * 상품 등록: 새우깡(100개/1600원), 콜라(500개/2000원), 천하장사(1000개/800원), 빼빼로(300개/1500원), 사이다(500/2000원) 
+		 * 2. 상품 등록: 새우깡(100개/1000원), 콜라(500개/2000원), 천하장사(1000개/800원), 빼빼로(300개/1500원), 사이다(500/2000원)
+		 * 창고에 저장된 수량 이상으로 저장될 경우는 결제 불가 
 		 */
 		
-		ProductClass p1 = new ProductClass(1, "새우깡", 1600, 100);
+		ProductClass p1 = new ProductClass(1, "새우깡", 1000, 100);
 		ProductClass p2 = new ProductClass(2, "콜라", 2000, 500);
 		ProductClass p3 = new ProductClass(3, "천하장사", 800, 1000);
-		ProductClass p4 = new ProductClass(4, "사이다", 2000, 500);
+		ProductClass p4 = new ProductClass(4, "빼빼로", 1500, 300);
+		ProductClass p5 = new ProductClass(5, "사이다", 2000, 500);
 		
 		p1.productInfo();
 		p2.productInfo();
 		p3.productInfo();
 		p4.productInfo();
+		
+		/*
+		 * 3. 상품 구매 - 장바구니에 담기
+		 */
+		c1.addToCart(p1, 10);
+//		c3.addToCart(물건, 수량);
+		
+		
+		/*
+		 * 상품 구매 - 결제
+		 */
+//		c3.purchase(); // ~가 새우깡을 구매했습니다.가 나오도록 하기
 		
 	}
 
